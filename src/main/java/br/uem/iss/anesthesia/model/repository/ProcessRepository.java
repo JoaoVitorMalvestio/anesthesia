d18f7ec4570939a71ff15392fbd79cda14afe673
mateuslu <mateuslupi94@gmail.com>
2019-10-27 16:29:59 -0200

    merge Mateus

---
+++ src/main/java/br/uem/iss/anesthesia/model/repository/ProcessRepository.java
@@ -0,0 +1,34 @@
+package br.uem.iss.anesthesia.model.repository;
+
+import br.uem.iss.anesthesia.model.entity.DoctorModel;
+import br.uem.iss.anesthesia.model.entity.PatientModel;
+import br.uem.iss.anesthesia.model.entity.ProcessModel;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+
+public interface ProcessRepository extends CrudRepository<ProcessModel, Long> {
+
+    Iterable<ProcessModel> findByActiveTrue();
+
+    Iterable<ProcessModel> findByDoctorAndActiveTrue(DoctorModel doctorModel);
+
+    Iterable<ProcessModel> findByPatientAndActiveTrue(PatientModel patientModel);
+
+    Iterable<ProcessModel> findByPatientAndDoctorAndActiveTrue(PatientModel patientModel, DoctorModel doctorModel);
+
+    Iterable<ProcessModel> findByPatientEquals(PatientModel patientModel);
+
+    Iterable<ProcessModel> findByInicialDateAfter(LocalDate calendar);
+
+    Iterable<ProcessModel> findByInicialDateBefore(LocalDate calendar);
+
+    Iterable<ProcessModel> findByInicialDateBeforeAndPatientEquals(LocalDate dtfinal, PatientModel patientModel);
+
+    Iterable<ProcessModel> findByInicialDateAfterAndPatientEquals(LocalDate dtinicial, PatientModel patientModel);
+
+    Iterable<ProcessModel> findByInicialDateAfterAndInicialDateBefore(LocalDate dtinicial, LocalDate dtfinal);
+
+    Iterable<ProcessModel> findByInicialDateAfterAndInicialDateBeforeAndPatientEquals(LocalDate dtinicial, LocalDate dtfinal, PatientModel patientModel);
+
+}
