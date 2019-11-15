fdac4619078ff7f5b3166acb282766ca85ae2fad
LarissaZanata <zanatalarissamorais@gmail.com>
2019-11-15 17:21:46 -0300

    Começando relatóriórios, listagem e ausencias

---
+++ src/main/java/br/uem/iss/anesthesia/controller/ReportController.java
@@ -0,0 +1,92 @@
+package br.uem.iss.anesthesia.controller;
+
+import br.uem.iss.anesthesia.controller.AbstractController;
+import br.uem.iss.anesthesia.controller.request.ProcessReportRequest;
+import br.uem.iss.anesthesia.model.entity.ProcessModel;
+import br.uem.iss.anesthesia.model.repository.AppointmentRepository;
+import br.uem.iss.anesthesia.model.repository.DoctorRepository;
+import br.uem.iss.anesthesia.model.repository.PatientRepository;
+import br.uem.iss.anesthesia.model.repository.ProcessRepository;
+import br.uem.iss.anesthesia.util.DateSupport;
+import br.uem.iss.anesthesia.view.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/report")
+public class ReportController extends AbstractController {
+
+    private PatientRepository patientRepository;
+    private DoctorRepository doctorRepository;
+    private ProcessRepository processRepository;
+    private AppointmentRepository appointmentRepository;
+    private DateSupport dateSupport;
+
+
+    public ReportController(PatientRepository patientRepository, DoctorRepository doctorRepository, AppointmentRepository appointmentRepository, DateSupport dateSupport, ProcessRepository processRepository) {
+        this.patientRepository = patientRepository;
+        this.doctorRepository = doctorRepository;
+        this.processRepository = processRepository;
+        this.appointmentRepository = appointmentRepository;
+        this.dateSupport = dateSupport;
+        this.processRepository = processRepository;
+    }
+
+    @GetMapping
+    public AbstractModelAndView home() {
+        return new AbstractModelAndView("report_index");
+    }
+
+    @GetMapping("/patient-registry/{id}")
+    public ModelAndView patientRegistry(@PathVariable Long id) {
+        return new PatientRegistryView(patientRepository.findById(id).get());
+    }
+
+    @GetMapping("/patient-report")
+    public AbstractModelAndView patientReport() {
+        return new PatientReportFormView(doctorRepository.findAll());
+    }
+
+    @PostMapping("/patient-report")
+
+
+    @GetMapping("/process-report")
+    public AbstractModelAndView formProcessReport() {
+        return new ProcessReportFormView(patientRepository.findAll(), new ProcessReportRequest(), null);
+    }
+
+    @PostMapping("/process-report")
+    public ModelAndView processReport(@ModelAttribute ProcessReportRequest request) {
+        Iterable<ProcessModel> process;
+        if(request.getInitial() != null && request.getEnd() != null && request.getPatient() != null){
+            process = processRepository.findByInicialDateAfterAndInicialDateBeforeAndPatientEquals(request.getInitial(), request.getEnd(), request.getPatient());
+        }else if(request.getInitial() != null && request.getEnd() != null ){
+            process = processRepository.findByInicialDateAfterAndInicialDateBefore(request.getInitial(), request.getEnd());
+        }else if(request.getInitial() != null && request.getPatient() != null ){
+            process = processRepository.findByInicialDateAfterAndPatientEquals(request.getInitial(), request.getPatient());
+        }else if(request.getEnd() != null && request.getPatient() != null){
+            process = processRepository.findByInicialDateBeforeAndPatientEquals(request.getInitial(), request.getPatient());
+        }else if(request.getEnd() != null){
+            process = processRepository.findByInicialDateBefore(request.getEnd());
+        }else if(request.getPatient() != null){
+            process = processRepository.findByPatientEquals(request.getPatient());
+        }else if (request.getInitial() != null){
+            process = processRepository.findByInicialDateAfter(request.getInitial());
+        }else{
+            process = processRepository.findByActiveTrue();
+        }
+        return new ProcessReportFormView(patientRepository.findAll(), request, process);
+    }
+
+    @GetMapping("/absence-report")
+    public AbstractModelAndView formAbsenceReport() {
+        return new AbsenseReportFormView(processRepository.findAll());
+    }
+
+    @GetMapping("/listagem-report")
+    public AbstractModelAndView listagenReport() {
+        AbstractModelAndView mv = new AbstractModelAndView("new_report_listagem");
+        return mv;
+    }
+}
---
+++ src/main/java/br/uem/iss/anesthesia/model/entity/ProcessModel.java
@@ -0,0 +1,104 @@
+package br.uem.iss.anesthesia.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name="Process")
+public class ProcessModel extends DefaultModel {
+
+    @OneToOne
+    private DoctorModel doctor;
+    @OneToOne
+    private PatientModel patient;
+    @OneToOne
+    private MedicalProcedureModel medicalProcedure;
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<ProcessExamsModel> procesexams;
+    private boolean active = true;
+    private LocalDate inicialDate;
+
+    public String getPacienteMedico(){
+        String texto = "Paciente: "+ this.getPatient().getName() + " | Médico: " + this.getDoctor().getName() ;
+        return texto;
+    }
+
+    public DoctorModel getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(DoctorModel doctor) {
+        this.doctor = doctor;
+    }
+
+    public PatientModel getPatient() {
+        return patient;
+    }
+
+    public void setPatient(PatientModel patient) {
+        this.patient = patient;
+    }
+
+    public LocalDate getInicialDate() {
+        return inicialDate;
+    }
+
+    public void setInicialDate(LocalDate inicialDate) {
+        this.inicialDate = inicialDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void inactivate() {
+        active = false;
+    }
+
+    public MedicalProcedureModel getMedicalProcedure() {
+        return medicalProcedure;
+    }
+
+    public void setMedicalProcedure(MedicalProcedureModel medicalProcedure) {
+        this.medicalProcedure = medicalProcedure;
+    }
+
+    public List<ProcessExamsModel> getProcesexams() {
+        return procesexams;
+    }
+
+    public void setProcesexams(List<ProcessExamsModel> procesexams) {
+        this.procesexams = procesexams;
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessModel{" +
+                "doctor=" + doctor +
+                ", patient=" + patient +
+                ", medicalProcedure=" + medicalProcedure +
+                ", procesexams=" + procesexams +
+                ", active=" + active +
+                ", inicialDate=" + inicialDate +
+                '}';
+    }
+
+    public ProcessModel(DoctorModel doctor, PatientModel patient, MedicalProcedureModel medicalProcedure, List<ProcessExamsModel> procesexams, boolean active) {
+        this.doctor = doctor;
+        this.patient = patient;
+        this.medicalProcedure = medicalProcedure;
+        this.procesexams = procesexams;
+        this.active = active;
+    }
+
+    public ProcessModel() {
+    }
+    public ProcessModel(Long id) {
+        this.setId(id);
+    }
+}
