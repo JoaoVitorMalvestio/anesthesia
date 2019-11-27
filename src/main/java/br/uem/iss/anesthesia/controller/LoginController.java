87904ff268f606f1d656f3b99092703318626956
João Vitor Malvestio da Silva <joaovitormalvestio@gmail.com>
2019-11-26 22:35:39 -0300

    Incluido menu para relatórios e feito view da relatorio visão diaria

--- src/main/java/br/uem/iss/anesthesia/controller/LoginController.java
+++ src/main/java/br/uem/iss/anesthesia/controller/LoginController.java
@@ -2,6 +2,7 @@ package br.uem.iss.anesthesia.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,4 +11,7 @@ public class LoginController {
     public ModelAndView home() {
         return new ModelAndView("layouts/app", "conteudo", "login");
     }
+
+    @PostMapping("")
+    public ModelAndView loginIn() { return new ModelAndView("layouts/app", "conteudo", "login"); }
 }
