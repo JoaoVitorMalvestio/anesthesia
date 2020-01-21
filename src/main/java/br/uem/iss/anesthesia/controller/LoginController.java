5ffe60dec665cefde9425a95a6b757e2152be8ee
João Vitor Malvestio da Silva <joaovitormalvestio@gmail.com>
2020-01-21 12:49:23 -0300

    Correção do Login e cadastro de usuário

--- src/main/java/br/uem/iss/anesthesia/controller/LoginController.java
+++ src/main/java/br/uem/iss/anesthesia/controller/LoginController.java
@@ -4,6 +4,7 @@ import br.uem.iss.anesthesia.model.business.LoginUserBusiness;
 import br.uem.iss.anesthesia.model.entity.UserModel;
 import br.uem.iss.anesthesia.view.HomeView;
 import br.uem.iss.anesthesia.view.LoginView;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import br.uem.iss.anesthesia.model.business.exception.BusinessRuleException;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(LoginController.HOME)
@@ -22,21 +25,33 @@ public class LoginController {
     private LoginUserBusiness loginUserBusiness;
 
     @GetMapping
-    public ModelAndView login() {
-        return viewWithoutMessage(new UserModel());
+    public ModelAndView login(HttpSession session) {
+        UserModel userLogado = (UserModel) session.getAttribute("usuarioLogado");
 
-        //return new ModelAndView("layouts/app", "conteudo", "login");
+        if (userLogado != null)
+            return new HomeView(null);
+
+        return viewWithoutMessage(new UserModel());
     }
 
     @PostMapping
-    public ModelAndView loginIn(UserModel user) {
+    public ModelAndView loginIn(UserModel user, HttpSession session) {
         try {
             loginUserBusiness.login(user);
-            return new HomeView(user,null);
+            session.setAttribute("usuarioLogado", user);
+            return new HomeView(null);
         } catch (BusinessRuleException e) {
             return viewWithMessage(user, e.getMessage());
         }
     }
+
+    @GetMapping("/logoff")
+    public ModelAndView logOff(HttpSession session) {
+        session.setAttribute("usuarioLogado", null);
+
+        return viewWithoutMessage(new UserModel());
+    }
+
 
     private LoginView viewWithoutMessage(UserModel user) {
         return viewWithMessage(user, null);
--- src/main/java/br/uem/iss/anesthesia/view/HomeView.java
+++ src/main/java/br/uem/iss/anesthesia/view/HomeView.java
@@ -1,11 +1,9 @@
 package br.uem.iss.anesthesia.view;
 
-import br.uem.iss.anesthesia.model.entity.UserModel;
-
 public class HomeView extends AbstractModelAndView {
 
-    public HomeView(UserModel user, String message) {
-        super("home", "user", user);
+    public HomeView(String message) {
+        super("home");
         this.addObject("message", message);
     }
 }
